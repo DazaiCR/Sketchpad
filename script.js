0075292c7d6900319e9cbb72c3430203ee1c40d3
DazaiCR <chaimaeattouche@gmail.com>
2025-01-07 14:16:49 +0100

    Add layout & grid creation logic

---
+++ script.js
@@ -0,0 +1,19 @@
+const myDivs = [];
+let numOfCols = 8;
+
+function createDiv() {
+    const gridDiv = document.createElement("div");
+    gridDiv.className = "square";
+    return gridDiv;
+}
+
+function fillGrid() {
+    const grid = document.querySelector(".grid");
+
+    for (let i = 0; i < numOfCols*numOfCols; i++) {
+      myDivs.push(createDiv());
+      grid.appendChild(myDivs[i]);
+    }
+}
+
+fillGrid();
