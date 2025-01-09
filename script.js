20d82f4912503feae1bc5d09fb61c83456163f31
DazaiCR <chaimaeattouche@gmail.com>
2025-01-09 19:13:32 +0100

    Sync clear and grid resize functionality with grid line visibility state & clear script file's structure

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let gridDivs = [];
 let numOfCols = 16;
 let gridLength = 620;
+let gridLines = 1;  // means they're not hidden
 let squares;
+let circle = document.querySelector(".circle");
+let gridSizeLabel = document.querySelector(".grid-size-label");
 let gridSizeInput = document.querySelector("#grid-size-input");
 let gridLinesButton = document.querySelector(".grid-lines");
-
+let clearButton = document.querySelector(".clear");
 
 function createDiv() {
     const gridDiv = document.createElement("div");
@@ -23,30 +26,19 @@ function fillGrid() {
     }
 }
 
-function setUpColorInputShadowColor() {
-    let circle = document.querySelector(".circle");
-    let inputVal = circle.value;
-
-    circle.addEventListener("input", () => {
-        inputVal = circle.value;
-        circle.style.boxShadow = `0 0 8px ${inputVal}`;
-    });
-}
-
-function setUpGridSizeValue() {
-    let gridSizeLabel = document.querySelector(".grid-size-label");
-    let inputSize = gridSizeInput.value;
-
-    gridSizeInput.addEventListener("input", () => {
-        inputSize = gridSizeInput.value;
-        numOfCols = inputSize;
-        gridSizeLabel.textContent = `${inputSize}x${inputSize}`;
-    });
-}
-
 fillGrid();
-setUpColorInputShadowColor();
-setUpGridSizeValue();
+
+circle.addEventListener("input", () => {
+    let inputVal = circle.value;
+    inputVal = circle.value;
+    circle.style.boxShadow = `0 0 8px ${inputVal}`;
+});
+
+gridSizeInput.addEventListener("input", () => {
+    let inputSize = gridSizeInput.value;
+    numOfCols = inputSize;
+    gridSizeLabel.textContent = `${inputSize}x${inputSize}`;
+});
 
 gridSizeInput.addEventListener("change", () => {
     let squareLength = gridLength / numOfCols;
@@ -56,9 +48,26 @@ gridSizeInput.addEventListener("change", () => {
         square.style.height = `${squareLength}px`;
         square.style.width = `${squareLength}px`;
     });
-})
+    if (!gridLines) {
+        squares.forEach(square => square.classList.add("no-borders"));
+    }
+});
 
 gridLinesButton.addEventListener("click", () => {
     squares = document.querySelectorAll(".square");
     squares.forEach(square => square.classList.toggle("no-borders"));
+    gridLines = (gridLines == 1) ? 0 : 1;
+});
+
+clearButton.addEventListener("click", () => {
+    let squareLength = gridLength / numOfCols;
+    fillGrid();
+    squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.style.height = `${squareLength}px`;
+        square.style.width = `${squareLength}px`;
+    });
+    if (!gridLines) {
+        squares.forEach(square => square.classList.add("no-borders"));
+    }
 });
