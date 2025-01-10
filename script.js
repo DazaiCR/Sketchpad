b2ae75c6a8136db11a96eaff82e12c839d2de1bf
DazaiCR <chaimaeattouche@gmail.com>
2025-01-10 23:07:40 +0100

    Add coloring functionalities & eraser mode

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let gridDivs = [];
 let numOfCols = 16;
 let gridLength = 620;
 let gridLines = 1;  // means they're not hidden
+let mouseDown = false;
 let squares;
 let circle = document.querySelector(".circle");
 let gridSizeLabel = document.querySelector(".grid-size-label");
 let gridSizeInput = document.querySelector("#grid-size-input");
 let gridLinesButton = document.querySelector(".grid-lines");
 let clearButton = document.querySelector(".clear");
+let exclusives = document.querySelectorAll(".exclusive");
+let colorButton = document.querySelector(".color-mode");
+let randomButton = document.querySelector(".random-mode");
+let shadowButton = document.querySelector(".shadow");
+let eraserButton = document.querySelector(".eraser");
+let grid = document.querySelector(".grid");
 
 function createDiv() {
     const gridDiv = document.createElement("div");
@@ -17,12 +23,10 @@ function createDiv() {
 
 function fillGrid() {
     const grid = document.querySelector(".grid");
-    gridDivs = [];
     grid.replaceChildren();
 
     for (let i = 0; i < numOfCols*numOfCols; i++) {
-      gridDivs.push(createDiv());
-      grid.appendChild(gridDivs[i]);
+      grid.appendChild(createDiv());
     }
 }
 
@@ -30,7 +34,6 @@ fillGrid();
 
 circle.addEventListener("input", () => {
     let inputVal = circle.value;
-    inputVal = circle.value;
     circle.style.boxShadow = `0 0 8px ${inputVal}`;
 });
 
@@ -67,7 +70,75 @@ clearButton.addEventListener("click", () => {
         square.style.height = `${squareLength}px`;
         square.style.width = `${squareLength}px`;
     });
-    if (!gridLines) {
+    if(!gridLines){
         squares.forEach(square => square.classList.add("no-borders"));
     }
 });
+
+exclusives.forEach( (exclusive) => {
+    exclusive.addEventListener("click", (event) => {
+        exclusives.forEach((exclusive) => {
+            if(event.target === exclusive){
+                exclusive.classList.toggle("exclusive-onclick");
+            }
+            else {
+                exclusive.classList.remove("exclusive-onclick");
+            }
+        });
+    });
+});
+
+grid.addEventListener("mousedown", (event) => {
+    if(event.target.classList.contains("square")){
+        mouseDown = true;
+        colorSquare(event.target);
+    }
+});
+
+grid.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+grid.addEventListener("mouseover", (event) => {
+    if(mouseDown && event.target.classList.contains("square")){
+        colorSquare(event.target);
+    }
+});
+
+function colorSquare(target) {
+    let color;
+    if(window.getComputedStyle(colorButton).color != "rgb(255, 255, 255)"){
+        color = circle.value;
+        target.style.opacity = 1;
+        target.style.backgroundColor = color;
+    }
+    else if(window.getComputedStyle(randomButton).color != "rgb(255, 255, 255)"){
+        color = getRandomColor();
+        target.style.opacity = 1;
+        target.style.backgroundColor = color;
+    }
+    else if(window.getComputedStyle(eraserButton).color != "rgb(255, 255, 255)"){
+        color = "black";
+        target.style.opacity = 1;
+        target.style.backgroundColor = color;
+    }
+    else if(window.getComputedStyle(shadowButton).color != "rgb(255, 255, 255)"){       
+        const color = "white";
+        let opacity = parseFloat(target.style.opacity) || 0;
+    
+        if (opacity < 1)
+            opacity += 0.1;
+        else if(opacity == 1)
+            opacity = 0.1;
+
+        target.style.backgroundColor = color;
+        target.style.opacity = opacity;
+    }
+}
+
+function getRandomColor() {
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
